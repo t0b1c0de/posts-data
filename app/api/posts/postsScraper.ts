9ef0bd7dd13be28c3a488333bb6c2b8eec130f8c
t0b1-code <rak4chris@gmail.com>
2025-04-23 15:56:10 +0300

    added a refer to the agent

--- app/api/posts/postsScraper.ts
+++ app/api/posts/postsScraper.ts
@@ -30,9 +30,10 @@ export default async function handler(url: string): Promise<ScrapedPost> {
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         Accept:
           "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Language": "en-US,en;q=0.0",
+        Referer: "https://www.google.com/",
       },
-      timeout: 10000, // 10 seconds timeout for the request
+      timeout: 12000, // 10 seconds timeout for the request
     });
 
     const $ = cheerio.load(response.data);
